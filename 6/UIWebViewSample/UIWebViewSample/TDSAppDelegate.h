66943f303f42a73b95fd0ac3b0f8941cd0fa0950
kterada <kterada@4cast.co.jp>
2014-07-27 11:58:53 +0900

    finish hw6

---
+++ 6/UIWebViewSample/UIWebViewSample/TDSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TDSAppDelegate.h
+//  UIWebViewSample
+//
+//  Created by kentaro terada on 2014/07/21.
+//  Copyright (c) 2014年 kentaro.terada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
